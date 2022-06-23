a23b8eace8f5f336bfb55842b4e5d8f55dbad1d3
sage <44532033+arpkirill@users.noreply.github.com>
2022-06-23 11:03:33 +0700

    Автоматическое форматирование линта

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,89 +1,74 @@
-<template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
+<template >
+  <v-container >
+    <v-row class="text-center" >
+      <v-col cols="12" >
         <v-img
           :src="require('../assets/logo.svg')"
           class="my-3"
           contain
-          height="200"
-        />
+          height="200" />
       </v-col>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
+      <v-col class="mb-4" >
+        <h1 class="display-2 font-weight-bold mb-3" >
           Welcome to the Vuetify 3 Beta
         </h1>
 
-
-        <p class="subheading font-weight-regular">
+        <p class="subheading font-weight-regular" >
           For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          <br />please join our online
+          <a href="https://community.vuetifyjs.com" target="_blank" >
+            Discord Community
+          </a>
         </p>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
+      <v-col class="mb-5" cols="12" >
+        <h2 class="headline font-weight-bold mb-5" >
           What's next?
         </h2>
 
-        <v-row justify="center">
+        <v-row justify="center" >
           <a
             v-for="(next, i) in whatsNext"
             :key="i"
             :href="next.href"
             class="subheading mx-3"
-            target="_blank"
-          >
+            target="_blank" >
             {{ next.text }}
           </a>
         </v-row>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
+      <v-col class="mb-5" cols="12" >
+        <h2 class="headline font-weight-bold mb-5" >
           Important Links
         </h2>
 
-        <v-row justify="center">
+        <v-row justify="center" >
           <a
             v-for="(link, i) in importantLinks"
             :key="i"
             :href="link.href"
             class="subheading mx-3"
-            target="_blank"
-          >
+            target="_blank" >
             {{ link.text }}
           </a>
         </v-row>
       </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
+      <v-col class="mb-5" cols="12" >
+        <h2 class="headline font-weight-bold mb-5" >
           Ecosystem
         </h2>
 
-        <v-row justify="center">
+        <v-row justify="center" >
           <a
             v-for="(eco, i) in ecosystem"
             :key="i"
             :href="eco.href"
             class="subheading mx-3"
-            target="_blank"
-          >
+            target="_blank" >
             {{ eco.text }}
           </a>
         </v-row>
@@ -93,11 +78,10 @@
 </template>
 
 <script>
-
 export default {
   name: 'HelloWorld',
 
-  data: () => ({
+  data: () => ( {
     ecosystem: [
       {
         text: 'vuetify-loader',
@@ -144,6 +128,6 @@ export default {
         href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
       },
     ],
-  }),
+  } ),
 }
 </script>
