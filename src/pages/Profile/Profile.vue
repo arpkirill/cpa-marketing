914f2d2ce082be5e9141e78336702cfdcbaee383
sage <44532033+arpkirill@users.noreply.github.com>
2022-07-08 16:17:22 +0700

    Update Profile.vue

--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -1,25 +1,29 @@
 <template >
   <v-container >
-    <v-card >
-      <v-tabs
-        v-model="tab"
-        light >
-        <v-tab :value="0" >
-          Личная информация
-        </v-tab>
-        <v-tab :value="1" >
-          Платежные реквизиты
-        </v-tab>
-      </v-tabs>
-      <v-window v-model="tab" >
-        <v-window-item :value="0" >
-          <ProfileInfo />
-        </v-window-item>
-        <v-window-item :value="1" >
-          <ProfileInfo />
-        </v-window-item>
-      </v-window>
-    </v-card>
+    <v-row >
+      <v-col >
+        <v-card >
+          <v-tabs
+            v-model="tab"
+            light >
+            <v-tab :value="0" >
+              Личная информация
+            </v-tab>
+            <v-tab :value="1" >
+              Платежные реквизиты
+            </v-tab>
+          </v-tabs>
+          <v-window v-model="tab" >
+            <v-window-item :value="0" >
+              <ProfileInfo />
+            </v-window-item>
+            <v-window-item :value="1" >
+              <ProfileInfo />
+            </v-window-item>
+          </v-window>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
