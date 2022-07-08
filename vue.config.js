682192edf210fc92a771c72ba5eb37a9bda4847b
sage <44532033+arpkirill@users.noreply.github.com>
2022-07-08 17:22:46 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require( '@vue/cli-service' )
 const path = require( 'path' )
 
 module.exports = defineConfig( {
+  publicPath: '/kirill.github.io/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
