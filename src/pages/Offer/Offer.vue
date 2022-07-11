a5836cc9b7660de9e0565942586fa6413b71c6ab
sage <44532033+arpkirill@users.noreply.github.com>
2022-07-11 16:12:07 +0700

    pdf viewer agent rules

--- src/pages/Offer/Offer.vue
+++ src/pages/Offer/Offer.vue
@@ -1,9 +1,16 @@
 <template >
-  <v-container > FAQ </v-container>
+  <v-container >
+    <pdf src="https://cdn.jsdelivr.net/gh/mozilla/pdf.js@c6e8ca86/test/pdfs/calrgb.pdf" />
+  </v-container>
 </template>
 
 <script>
+import pdf from '@jbtje/vue3pdf'
+
 export default {
   name: 'FAQ',
+  components: {
+    pdf,
+  },
 }
 </script>
