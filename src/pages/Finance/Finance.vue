165ba6757ac1ec476008ef9d6aaf02e842c4f57b
sage <44532033+arpkirill@users.noreply.github.com>
2022-07-11 15:16:06 +0700

    finance template

--- src/pages/Finance/Finance.vue
+++ src/pages/Finance/Finance.vue
@@ -1,24 +1,78 @@
 <template >
   <v-container fluid >
     <v-row >
+      <v-col
+        cols="12"
+        sm="6" >
+        <v-card class="pa-3" >
+          <v-card-title >
+            <v-spacer />
+            <div class="text-center" >
+              <v-icon size="64" >
+                mdi-chevron-right
+              </v-icon>
+              <div >Текущий баланс</div>
+            </div>
+            <v-spacer />
+          </v-card-title>
+          <v-card-text >
+            <div class="text-h5 text-center" >
+              50₽
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="6" >
+        <v-card class="pa-3" >
+          <v-card-title >
+            <v-spacer />
+            <div class="text-center" >
+              <v-icon size="64" >
+                mdi-chevron-right
+              </v-icon>
+              <div >Заработано за все время</div>
+            </div>
+            <v-spacer />
+          </v-card-title>
+          <v-card-text >
+            <div class="text-h5 text-center" >
+              100₽
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row >
       <v-col >
         <v-table >
-          <thead >
+          <thead class="pl-2" >
             <tr >
               <th class="text-left" >
-                Name
+                ID
               </th>
               <th class="text-left" >
-                Calories
+                Дата выплаты
+              </th>
+              <th class="text-left" >
+                Статус
+              </th>
+              <th class="text-left" >
+                Сумма
               </th>
             </tr>
           </thead>
           <tbody >
             <tr
-              v-for="item in desserts"
-              :key="item.name" >
-              <td >{{ item.name }}</td>
-              <td >{{ item.calories }}</td>
+              v-for="item in partners"
+              :key="item.id" >
+              <td class="pa-4" >
+                {{ item.id }}
+              </td>
+              <td >{{ item.registrationDate }}</td>
+              <td >{{ item.status }}</td>
+              <td >{{ item.amountOfIncome }}₽</td>
             </tr>
           </tbody>
         </v-table>
@@ -28,53 +82,16 @@
 </template>
 
 <script>
+import mock from '../Finance/mock'
+
 export default {
   name: 'Finance',
   data() {
     return {
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-        },
-      ],
+      partners: mock,
     }
   },
 }
 </script>
+
+<style lang="scss"></style>
