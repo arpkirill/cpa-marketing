9a942fdb027eb034d89948f31294b65de0779512
sage <44532033+arpkirill@users.noreply.github.com>
2022-07-13 16:37:27 +0700

    CookieLaw

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
   <v-app >
     <router-view />
     <Snackbar />
+    <CookieLaw />
   </v-app>
 </template>
 
 <script>
 import Snackbar from '@/components/Snackbar/Snackbar.vue'
+import CookieLaw from '@/components/CookieLaw/CookieLaw.vue'
 
 export default {
   name: 'App',
-  components: { Snackbar },
+  components: { Snackbar, CookieLaw },
 }
 </script>
 
---
+++ src/components/CookieLaw/CookieLaw.vue
@@ -0,0 +1,42 @@
+<template >
+  <v-snackbar
+    v-model="isOpen"
+    timeout="-1"
+    multi-line >
+    Этот веб-сайт использует файлы cookie, чтобы обеспечить Вам наилучший опыт работы на нашем веб-сайте.
+    <template v-slot:actions >
+      <v-btn
+        color="red"
+        variant="text"
+        @click="accept" >
+        Принять все
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: 'CookieLaw',
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+  created() {
+    this.isOpen = this.isAccepted()
+  },
+  methods: {
+    accept() {
+      this.setAccepted()
+    },
+    setAccepted() {
+      localStorage.setItem( 'cookie:accepted', true )
+      this.isOpen = false
+    },
+    isAccepted() {
+      return localStorage.getItem( 'cookie:accepted' ) === 'true' ? false : true
+    },
+  },
+}
+</script>
